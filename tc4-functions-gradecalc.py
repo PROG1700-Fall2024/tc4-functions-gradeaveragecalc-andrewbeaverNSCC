3c677f076a8cef8b3db99fddf65bcb890f18d144
Your Name <w0402993@nscc.ca>
2024-11-07 11:56:20 -0400

    TC4 Complete

--- tc4-functions-gradecalc.py
+++ tc4-functions-gradecalc.py
@@ -4,24 +4,24 @@
 # to calculate a final grade point average for all six courses.
 ############################################
 
-# Student Name: 
+# Student Name: Andrew Beaver
 
-# main() FUNCTION
-def main():
+numericGrade = 0.0
 
+def openingMessage():
     print("Grade Point Calculator\n")
     print("Valid letter grades that can be entered: A, B, C, D, F.")
     print("Valid grade modifiers are +, - or nothing.")
     print("All letter grades except F can include a + or - symbol.")
-    print("Calculated grade point value cannot exceed 4.0.\n")
+    print("Calculated grade point value cannot exceed 4.0.")
 
-    numericGrade = 0.0
+def letterInput(p_course):
+    return input("\nPlease enter a letter grade for " + p_course + ": ").upper()
 
-    #Gather user inputs
-    letterGrade = input("Please enter a letter grade : ").upper()
-    modifier = input("Please enter a modifier (+, - or nothing) : ")
+def modifierInput():
+    return input("Please enter a modifier (+, - or nothing) : ")
 
-    # Determine base numeric value of the grade
+def calculateGrade(letterGrade, modifier):
     if letterGrade == "A":
         numericGrade = 4.0
     elif letterGrade == "B":
@@ -35,7 +35,7 @@ def main():
     else:
         #If an invalid entry is made
         print("You entered an invalid letter grade.")
-    
+
     # Determine whether to apply a modifier
     if modifier == "+":
         if letterGrade != "A" and letterGrade != "F": # Plus is not valid on A or F
@@ -43,9 +43,61 @@ def main():
     elif modifier == "-":
         if letterGrade != "F":     # Minus is not valid on F
             numericGrade -= 0.3
+    return numericGrade
 
-    # Output final message and result, with formatting
-    print("The numeric value is: {0:.1f}".format(numericGrade))
+# main() FUNCTION
+def main():
+    openingMessage()
+
+    #prog calls
+    progLetterGrade = letterInput("PROG1700")
+    progModifier = modifierInput()
+
+    progfinal = calculateGrade(progLetterGrade, progModifier)
+    
+    #NETW calls
+    NetWLetterGrade = letterInput("NETW1700")
+    NetWModifier = modifierInput()
+
+    NETWFinal = calculateGrade(NetWLetterGrade, NetWModifier)
+
+    #OSYS calls
+    OSYSLetterGrade = letterInput("OSYS1200")
+    OSYSModifier = modifierInput()
+
+    OSYSFinal = calculateGrade(OSYSLetterGrade, OSYSModifier)
+
+    #WEBD Calls
+    WEBDLetterGrade = letterInput("WEBD1000")
+    WEBDModifier = modifierInput()
+
+    WEBDFinal = calculateGrade(WEBDLetterGrade, WEBDModifier)
+
+    #COMM1700 calls
+    COMMLetterGrade = letterInput("COMM1700")
+    COMMModifier = modifierInput()
+
+    COMMFinal = calculateGrade(COMMLetterGrade, COMMModifier)
+
+    #DBAS calls
+    DBASLetterGrade = letterInput("DBAS1007")
+    DBASModifier = modifierInput()
+
+    DBASFinal = calculateGrade(DBASLetterGrade, DBASModifier)
+
+    # Output course results, with formatting
+    print("\nThe final grade for PROG1700 is: {0:.1f}".format(progfinal))
+    print("The final grade for NETW1700  is: {0:.1f}".format(NETWFinal))
+    print("The final grade for OSYS1200  is: {0:.1f}".format(OSYSFinal))
+    print("The final grade for WEBD1000  is: {0:.1f}".format(WEBDFinal))
+    print("The final grade for COMM1700  is: {0:.1f}".format(COMMFinal))
+    print("The final grade for DBAS1007  is: {0:.1f}".format(DBASFinal))
+
+    #GPA math
+    GPA = (progfinal + NETWFinal + OSYSFinal + WEBDFinal + COMMFinal + DBASFinal) / 6
+
+    #Display GPA
+    print("\nYour grade point average for the semester is: {0:.1f}".format(GPA))
 
 #PROGRAM EXECUTION STARTS HERE
 main()
